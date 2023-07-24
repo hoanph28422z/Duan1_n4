ade1119b874a509aded45e349d4d51812eba39aa
hoanph28422z <hoanndph28422@fpt.edu.vn>
2023-07-24 17:51:23 +0700

    quan ly nguoi dung

---
+++ admin/taikhoan/list.php
@@ -0,0 +1,55 @@
+<div class="row">
+            <div class="row margin-bottom  form-title"><h1>DANH SÁCH TÀI KHOẢN</h1></div>
+
+            <div class="row form-content">
+                
+                    <div class="row margin-bottom10 form-dsLOAI">
+                            <table>
+                                    <tr>
+                                        <th></th>
+                                        <th>MÃ TK</th>
+                                        <th>USERNAME</th>
+                                        <th>EMAIL</th>
+                                        <th>PASSWORD</th>
+                                        <th>ROLE</th>
+                                        <th></th>
+                                    </tr>
+
+                                  
+                                    <?php
+                                foreach ($dsTK as $tk) {
+                                    extract($tk);
+                                    $suatk = "index.php?act=suatk&makh=".$ma_kh;
+                                    $xoatk= "index.php?act=xoatk&makh=".$ma_kh;
+                                    echo 
+                                        '<tr>
+                                        <td><input type="checkbox" name="" id=""></td>
+                                        <td>'.$ma_kh.'</td>
+                                        <td>'.$ho_ten.'</td>    
+                                        <td>'.$email.'</td>    
+                                        <td>'.$mat_khau.'</td>    
+                                        <td>'.$vai_tro.'</td>    
+                                        <td><a href="'.$suatk.'"><input type="button" value="Sửa"></a> 
+                                          <a href="'.$xoatk.'"><input type="button" value="Xóa"></a></td>
+                                        </tr>';
+
+                                }
+
+                                ?>
+                                    
+                            </table>
+                    </div>
+
+                    <div class="row margin-bottom10">
+                            <input type="button" value="Chọn tất cả">
+                            <input type="button" value="Bỏ chọn tất cả">
+                            <input type="button" value="Xóa các mục đã chọn">
+                        
+                    </div>
+
+                       
+               
+        </div>
+
+
+    </div>
---
+++ admin/taikhoan/update.php
@@ -0,0 +1,63 @@
+
+
+<div class="row">
+            <div class="row margin-bottom  form-title"><h1>CẬP NHẬT TÀI KHOẢN</h1></div>
+
+            <div class="row form-content">
+                
+            <form action="index.php?act=capnhatKH" method="post" class="form-quanlykh">
+                    
+                    <div class="row margin-bottom10">
+                        <label for="">USERNAME</label> <br>
+                        <input type="text" name="tenkh" value="<?=$ho_ten ?>"> 
+                    </div>
+
+
+                    
+
+                    <div class="row margin-bottom10">
+                        <label for="">EMAIL</label> <br>
+                        <input type="text" name="email" value="<?=$email ?>"> 
+                    </div>
+
+
+                    <div class="row margin-bottom10">
+                        <label for="">PASSWORD</label> <br>
+                        <input type="text" name="pass" value="<?=$mat_khau ?>"> 
+                    </div>
+
+
+                    
+
+
+                    <div class="row margin-bottom10">
+                        <label for="choose">ROLE</label> <br>
+                      
+                        
+                        <input type="text" name="role" value="<?=$vai_tro ?>"> 
+                        
+                    </div>
+
+
+                
+                    <div>
+                            <input type="hidden" name="ma_kh" value="<?=$ma_kh ?>">
+                            <input type="submit" name="capnhat" value="CẬP NHẬT">
+                            <a href="index.php?act=dskh"><input type="button" value="DANH SÁCH"></a>
+                    </div>
+
+                    
+
+                    <?php 
+                           if (isset($thongbao) && ($thongbao!="")) {
+                            echo $thongbao;
+                           }
+                            
+                            ?>
+
+                       
+                </form>
+                    
+
+
+    </div>
