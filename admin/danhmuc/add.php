3c4a9d9c5d990cc71924e644847c416a7a21d726
Anhnv28374 <anhnvph28374@fpt.edu.vn>
2023-08-02 23:04:40 +0700

    anhnv

--- admin/danhmuc/add.php
+++ admin/danhmuc/add.php
@@ -1,32 +1,94 @@
+<style>
+        .title {
+                text-transform: capitalize;
+        }
 
+        #form_group {
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                width: 450px;
+                margin: 0 auto;
+
+        }
+
+        .form_tag {
+                width: 555px;
+                height: 560px;
+                position: fixed;
+                top: 0%;
+                left: 20%;
+                box-shadow: 1px 1px 10px rgba(24 15 24 /25%); 
+                height: 100vh;
+                width:80% ;
+
+        }
+
+        .hiden {
+                height: 45px;
+                text-align: center;
+                text-transform: uppercase;
+                border-radius: 5px;
+                border: none;
+                box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.5);
+                font-weight: 550;
+
+        }
+
+        .name_tag {
+                width: 100%;
+                padding: 15px;
+                border: none;
+                outline: none;
+                border-radius: 5px;
+                margin: 15px 0;
+                font-weight: 550;
+
+        }
+
+        .btn_tag {
+                border: none;
+                outline: none;
+                box-shadow: 1px 1px 10px rgba(24 15 24 /25%);
+                padding: 15px;
+                border-radius: 50px;
+                margin: 15px 10px;
+                cursor: pointer;
+                font-weight: 550;
+        }
+        .btn_tag:hover {
+                transform: translateY(-5px);
+                transition: 0.2s all linear;
+        }
+</style>
 <div class="row">
-            <div class="row  form-title"><h1>Thêm mới loại hàng hóa</h1></div>
-            <div class="row form-content">
-                    <form action="index.php?act=adddm" method="post">
-                        <div class="row margin-bottom10">
-                            <label for="">Mã loại</label>
-                            <input type="text" name="maloai" placeholder="auto number" disabled> 
+        <div class="row form-content">
+                <form action="index.php?act=adddm" method="post" class="form_tag">
+                        <br>
+                        <h1 class="title">Thêm mới loại hàng hóa</h1>
+                        <br>
+                        <div class="row margin-bottom10" id="form_group ">
+                                <input type="text" class="hiden" name="maloai" placeholder="Tên Loại" disabled>
                         </div>
 
-                        <div class="row margin-bottom10">
-                            <label for="">Tên loại</label>
-                            <input type="text" name="tenloai"> 
+                        <div class="row margin-bottom10" id="form_group">
+                                <input type="text" class="name_tag" name="tenloai" placeholder="Mời Bạn Nhập Dữ Liệu">
                         </div>
 
                         <div class="row margin-bottom">
-                                <input type="submit" name="themmoi" value="THÊM MỚI">
-                                <input type="reset" value="NHẬP LẠI">
-                                <a href="index.php?act=listdm"><input type="button" value="DANH SÁCH"></a>
+                                <input type="submit" class="btn_tag" name="themmoi" value="THÊM MỚI">
+                                <input type="reset" class="btn_tag" value="NHẬP LẠI">
+                                <a href="index.php?act=listdm"><input type="button" class="btn_tag" value="DANH SÁCH"></a>
                         </div>
 
-                           <?php 
-                           if (isset($thongbao)&&($thongbao!="")) {
-                            echo $thongbao;
-                           }
-                            
-                            ?>
-                    </form>
-            </div>
-    </div>
+                        <?php
+                        if (isset($thongbao) && ($thongbao != "")) {
+                                echo $thongbao;
+                        }
 
-    </div>
+                        ?>
+                </form>
+        </div>
+</div>
+
+</div>
--- admin/taikhoan/list.php
+++ admin/taikhoan/list.php
@@ -1,55 +1,83 @@
+<style>
+.row {
+    position: relative;
+    left: 102px;
+    width: 79vw;
+
+}
+.text_center tr th,
+.text_center tr td{
+    text-align: center;
+    text-transform: uppercase;
+}
+
+.btn_user {
+    background: none;
+    border: none;
+    box-shadow: 1px 1px 10px rgba(22 25 19 /15%);
+    padding: 10px 15px;
+    border-radius: 15px;
+    margin: 0 10px;
+    cursor: pointer;
+}
+
+.text_user {
+    display: flex;
+    justify-content: center;
+
+}
+</style>
+
+
 <div class="row">
-            <div class="row margin-bottom  form-title"><h1>DANH SÁCH TÀI KHOẢN</h1></div>
+    <div class="row margin-bottom  form-title">
+        <h1>DANH SÁCH TÀI KHOẢN</h1>
+    </div>
 
-            <div class="row form-content">
-                
-                    <div class="row margin-bottom10 form-dsLOAI">
-                            <table>
-                                    <tr>
-                                        <th></th>
-                                        <th>MÃ TK</th>
-                                        <th>USERNAME</th>
-                                        <th>EMAIL</th>
-                                        <th>PASSWORD</th>
-                                        <th>ROLE</th>
-                                        <th></th>
-                                    </tr>
+    <div class="row form-content">
 
-                                  
-                                    <?php
+        <div class="row margin-bottom10 form-dsLOAI">
+            <table class="text_center">
+                <tr>
+
+                    <th>MÃ TK</th>
+                    <th>USERNAME</th>
+                    <th>EMAIL</th>
+                    <th>PASSWORD</th>
+                    <th>ROLE</th>
+                    <th>Chức Năng</th>
+                </tr>
+
+
+                <?php
                                 foreach ($dsTK as $tk) {
                                     extract($tk);
                                     $suatk = "index.php?act=suatk&makh=".$ma_kh;
                                     $xoatk= "index.php?act=xoatk&makh=".$ma_kh;
                                     echo 
                                         '<tr>
-                                        <td><input type="checkbox" name="" id=""></td>
+
                                         <td>'.$ma_kh.'</td>
                                         <td>'.$ho_ten.'</td>    
                                         <td>'.$email.'</td>    
                                         <td>'.$mat_khau.'</td>    
                                         <td>'.$vai_tro.'</td>    
-                                        <td><a href="'.$suatk.'"><input type="button" value="Sửa"></a> 
-                                          <a href="'.$xoatk.'"><input type="button" value="Xóa"></a></td>
+                                        <td class="text_user">
+                                        <a href="'.$suatk.'"><input type="button" class="btn_user" value="Sửa"></a> 
+                                          <a href="'.$xoatk.'"><input type="button" class="btn_user" value="Xóa"></a></td>
                                         </tr>';
 
                                 }
 
                                 ?>
-                                    
-                            </table>
-                    </div>
 
-                    <div class="row margin-bottom10">
-                            <input type="button" value="Chọn tất cả">
-                            <input type="button" value="Bỏ chọn tất cả">
-                            <input type="button" value="Xóa các mục đã chọn">
-                        
-                    </div>
-
-                       
-               
+            </table>
         </div>
 
 
+
+
     </div>
+
+
+</div>
